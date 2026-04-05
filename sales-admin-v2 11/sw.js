dbfafd2431943f781bc5921fc9c71cab9d2b69a9
tsugawa03 <tsugawa.agent@gmail.com>
2026-04-05 19:56:51 +0900

    Add files via upload

---
+++ sales-admin-v2 11/sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'sales-admin-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => {
+    return fetch(e.request).then(res => {
+      if (res && res.status === 200) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => cached);
+  }));
+});
